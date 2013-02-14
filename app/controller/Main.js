fa4421bfbc19c91526312827c140901b062f9381
unknown <anish@rims015.mjsindia.com>
2013-02-14 17:26:23 +0530

    device fix

--- app/controller/Main.js
+++ app/controller/Main.js
@@ -838,7 +838,7 @@ Ext.define('radiss.controller.Main', {
 						}
 			
 			navigator.camera.getPicture(onPhotoDataSuccess, onFail, { quality: 50,
-        	destinationType: destinationType.DATA_URL });
+        	destinationType: navigator.camera.DestinationType.DATA_URL});
         	var attachMentObj=btn.up('panel');//Ext.create('radiss.view.NewAttachmentForm');
         	console.log(attachMentObj.getHtml());
         	btn.setText('Take another picture');
